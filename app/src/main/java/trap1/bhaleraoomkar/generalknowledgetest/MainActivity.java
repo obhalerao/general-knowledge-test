f16a861f5b8a232295abc3c3f5375a1ca91206d9
obhalerao <obhal6543@gmail.com>
2019-09-26 10:10:57 -0400

    Added A TON more questions (70 in total) Implemented randomization of questions Slightly increased time limit per questions to 20.

--- app/src/main/java/trap1/bhaleraoomkar/generalknowledgetest/MainActivity.java
+++ app/src/main/java/trap1/bhaleraoomkar/generalknowledgetest/MainActivity.java
@@ -42,18 +42,82 @@ public class MainActivity extends AppCompatActivity {
             "Which computer scientist created Algorithm X? (Last name only)",
             "The Moon orbits the Earth at the same rate as its rotation. This is known as ___ ___",
             "To the nearest thousand, what is the speed of light in kilometers per second?",
-            "What year did Thomas Jefferson die?"
+            "What year did Thomas Jefferson die?",
+            "In what year did TJ's latest renovation finish?",
+            "What is the largest organ in the human body?",
+            "Which class won TJ Homecoming 2018?",
+            "What is the musical note a perfect fourth up from C?",
+            "What is the country with the highest population density?",
+            "What is the most electronegative element on the periodic table?",
+            "What state is Washington University in?",
+            "How many fluid ounces are in a gallon?",
+            "How many cubic meters of dirt are in a 5 meter by 2 meter by 1 meter hole?",
+            "How many keys are on a piano?",
+            "The longest river in the world is the ___ River.",
+            "On what planet have the lowest temperatures in the Solar System been reached?",
+            "The oldest Ivy League university is ___ University.",
+            "The youngest Ivy League university is ___ University.",
+            "What country fully surrounds Lesotho?",
+            "Which writer wrote 'The Jungle Book,' along with other famous works? (Last name only)",
+            "What is the name of the body of water separating the UK and France?",
+            "What is 2 + 2?",
+            "The Chicxulub event occurred about __ million years ago",
+            "When was Java first created?",
+            "What is the name of TJ's principal from 2006 to 2017? (Last name only)",
+            "The ring of volcanoes that encircles the Pacific Ocean is known as the Pacific Ring of ___.",
+            "How many 1's are in the 5x5 identity matrix?",
+            "What year did World War II end?",
+            "What is the operation after exponentiaion in the hyperoperation sequence?",
+            "What number is famous for using the hyperoperation sequence to become very, very large?",
+            "On an equal-temperament tuned instrument, a minor second involves multiplying the frequency by the __th root of 2.",
+            "The name of the most commonly used Android layout is the ____ Layout.",
+            "The only two doubly landlocked countries are Lichtenstein and ___.",
+            "The body of rocks between Mars and Jupiter is known as the ___ Belt.",
+            "Jeff Bezos, the current richest man in the world, is the CEO of ___.",
+            "What is the capital of North Korea?",
+            "What is the hardest naturally occuring mineral?",
+            "A cipher which involves a shift in the alphabet is also known as a ___ cipher.",
+            "The deepest lake in the world is Lake ___.",
+            "Crater Lake is in which state?",
+            "To the nearest 10,000 kilometers, what is the circumference of the Earth?",
+            "How old is the average student when they begin their first year of high school?",
+            "How many bonds are in benzene?",
+            "What is the largest country by area?",
+            "The moon's gravity is about 1/_ as strong as Earth's.",
+            "What is the 'number of the beast?'",
+            "In 2010, a 7.0 magnitude earthquake hit which Caribbean country?",
+            "The group that claimed responsibility for 9/11 was al-___.",
+            "What is 3 + 3?",
+            "What is 4 + 4?",
+            "The circle above 66.5 degrees North latitude is known as the ___ Circle.",
+            "Java has ___ typing, as opposed to Python's dynamic typing.",
+            "How many degrees are in a circle?",
+            "What is the interior angle of a pentagon in degrees?",
+            "What is the southernmost U.S. state?",
+            "What is the age of majority in Canaada?",
+            "What is 5 + 5?",
+            "What is 0 + 0?",
+            "What is e^(i * pi)?"
     };
-    String[] answers = new String[]{"2","Illinois","1865","80","Capulet","118","8","Even","Strunk", "Tra", "23", "Knuth", "Tidal Lock", "300000", "1826"};
+    String[] answers = new String[]
+            {"2","Illinois","1865","80","Capulet","118","8","Even","Strunk", "Tra", "23", "Knuth", "Tidal Lock", "300000", "1826","2017",
+    "Skin","2019","F","Monaco","Fluorine","Missouri","128","0","88","Nile","Earth","Harvard","Cornell","South Africa","Kipling","English Channel",
+                    "4","66","1995","Glazer","Fire","5","1945","Tetration","Graham's Number","12","Constraint","Uzbekistan","Asteroid","Amazon","Pyongyang",
+                    "Diamond","Caesar","Baikal","Oregon","40000","14","6","Russia","6","666","Haiti","Qaeda","6","8","Arctic","Static","360","108","Hawaii","19",
+                    "10","0","-1"};
     TreeSet<Entry> leaderboard = new TreeSet<Entry>(Collections.<Entry>reverseOrder());
     String playerName = "";
+    int numQuestions = 10;
     int pointVal = 1000;
     int currIdx = -1;
     String currAns = "";
     int score = 0;
     Timer timer = new Timer();
-    int max_duration = 1000;
+    int max_duration = 2000;
     int duration = max_duration;
+
+    HashSet<Integer> seen = new HashSet<Integer>();
+    ArrayList<Integer> indices = new ArrayList<Integer>();
 
     DecimalFormat formatTime = new DecimalFormat("0.0");
 
@@ -65,6 +129,16 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener reset = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            seen = new HashSet<Integer>();
+            indices = new ArrayList<Integer>();
+            int curr = -1;
+            for(int i = 0; i < numQuestions; i++){
+                do {
+                    curr = (int) (Math.random() * questions.length);
+                }while(seen.contains(curr));
+                indices.add(curr);
+                seen.add(curr);
+            }
             score = 0;
             currIdx = -1;
             duration = max_duration;
@@ -164,7 +238,7 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if(currIdx >= questions.length){
+                            if(currIdx >= numQuestions){
                                 timer.cancel();
 
                             }
@@ -172,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
                                 duration--;
                                 String currentTime = getString(R.string.time,""+formatTime.format((double)(duration/100.0)));
                                 timeLeft.setText(currentTime);
-                                String currentPoints = getString(R.string.points, ""+Math.round((double) pointVal * (.6 + .4 * ((double) duration / (double) (max_duration)))));
+                                String currentPoints = getString(R.string.points, ""+Math.round((double) pointVal * (.5 + .5 * ((double) duration / (double) (max_duration)))));
                                 points.setText(currentPoints);
                             }
                             if(duration<=0){
@@ -189,11 +263,11 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             }, 0, 10);
-            if(currIdx >= questions.length){
+            if(currIdx >= numQuestions){
                 end.onClick(v);
             }else {
                 entry1.setEnabled(true);
-                text1.setText(String.format("Question %s:\n%s", currIdx + 1, questions[currIdx]));
+                text1.setText(String.format("Question %s:\n%s", currIdx + 1, questions[indices.get(currIdx)]));
                 button1.setText(getString(R.string.click_button));
                 button1.setOnClickListener(response);
             }
@@ -213,15 +287,15 @@ public class MainActivity extends AppCompatActivity {
             points.setVisibility(View.GONE);
             entry1.setEnabled(false);
             entry1.setText("");
-            if (currAns.equals(answers[currIdx].toLowerCase()) && duration > 0) {
+            if (currAns.equals(answers[indices.get(currIdx)].toLowerCase()) && duration > 0) {
                 layout.setBackgroundColor(Color.GREEN);
-                long points = Math.round((double)pointVal * (.6 + .4*((double)duration/(double)(max_duration))));
+                long points = Math.round((double)pointVal * (.5 + .5*((double)duration/(double)(max_duration))));
                 text1.setText(getString(R.string.right, points));
                 score += points;
 
             } else {
                 layout.setBackgroundColor(Color.RED);
-                text1.setText(String.format("%s The correct answer is %s.", getString(R.string.wrong), answers[currIdx]));
+                text1.setText(String.format("%s The correct answer is %s.", getString(R.string.wrong), answers[indices.get(currIdx)]));
             }
             scorebox.setText(String.format("Score: %s", score));
             button1.setText(getString(R.string.click_button_continue));
